ab0ac2adeddd862c480452796ec69accfd322764
Kenzo-Wada <wada-kenzo@ed.tmu.ac.jp>
2022-06-14 19:15:45 +0900

    :thumbsup: add props into Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next'
-import { AppBar, Toolbar, Box } from '@mui/material'
+import { AppBar, Toolbar, Box, Typography } from '@mui/material'
 
-const Header: NextPage = () => {
+interface Props {
+    title: String
+}
+
+const Header: NextPage<Props> = ({title}) => {
     return (
-        <Box style={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    sample text
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar color='inherit' position="static" elevation={0} sx={{borderBottom: 1, borderBottomColor:'#f5f6f6'}} >
+                <Toolbar sx={{display:'flex',justifyContent: 'center'}}>
+                    <Typography sx={{fontWeight:'bold'}}>{title}</Typography>
                 </Toolbar>
             </AppBar>
         </Box>
