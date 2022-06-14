0a0b63e6e4cd8933f584aed220b7afcbcf2e70a7
Keiichi-S <keiichidori1222@gmail.com>
2022-06-15 01:08:14 +0900

    can chat

--- pages/[chatRoom].tsx
+++ pages/[chatRoom].tsx
@@ -1,15 +1,17 @@
 import { NextPage } from "next";
 import Header from "../components/header";
 import { useEffect, useState } from "react";
-import { setupSocket, sendMessage } from "../utils/socket";
+import { setupSocket, sendMessage, chatInit } from "../utils/socket";
 import { useRouter } from "next/router";
 
 const ChatRoom: NextPage = () => {
+    const [messageList, setMessageList] = useState<string[]>([]);
     const [message, setMessage] = useState<string>("");
     const router = useRouter()
     const chatRoom = router.query.chatRoom as string;
     useEffect(() => {
         setupSocket(chatRoom);
+        chatInit(setMessageList);
     }, [chatRoom]);
     return(
         <>
@@ -17,10 +19,13 @@ const ChatRoom: NextPage = () => {
             <textarea value={message} onChange={(event) => setMessage(event.target.value)}></textarea>
             <button onClick={() => {
                 sendMessage(chatRoom, message)
+                setMessage("")
             }}>
                 送信
             </button>
-            <div>chat page</div>
+            {messageList.map((message, i) => (
+                <p key={i}>{message}</p>
+            ))}
         </>
     )
 }
--- utils/socket.ts
+++ utils/socket.ts
@@ -1,19 +1,21 @@
+import { Dispatch, SetStateAction } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 let socket: Socket;
 let chatMessages: string[] = [];
+let setChatMessages: Dispatch<SetStateAction<string[]>>;
 
 const setupSocket = async (
     chatRoom: string
 ) => {
     socket = io("localhost:8080").connect();
-    socket.emit("join", {"username": "taro", "room": chatRoom})
+    socket.emit("join", "taro", chatRoom)
 
     socket.on("message", handleMessage);
 };
 
-const handleMessage = (message: string) => {
-    console.log(message)
+const chatInit = (setMessages: Dispatch<SetStateAction<string[]>>) => {
+    setChatMessages = setMessages;
 }
 
 const sendMessage = (chatRoom: string, message: string) => {
@@ -21,5 +23,11 @@ const sendMessage = (chatRoom: string, message: string) => {
     socket.emit("message", chatRoom, message)
 }
 
+const handleMessage = (message: string) => {
+    chatMessages = chatMessages.concat(message);
+    setChatMessages(chatMessages);
+}
 
-export {setupSocket, sendMessage}
+
+
+export {setupSocket, chatInit, sendMessage}
