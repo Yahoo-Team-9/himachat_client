fd421739bcaa185f8d59e167442ac77b6d71ccf0
Kenzo-Wada <wada-kenzo@ed.tmu.ac.jp>
2022-06-14 15:02:34 +0900

    :rocket: add some introduction template

---
+++ components/ChatList/ChatUser.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const ChatUser: NextPage = () => {
+    return(
+        <div>
+            sample component
+        </div>
+    )
+}
+
+export default ChatUser
