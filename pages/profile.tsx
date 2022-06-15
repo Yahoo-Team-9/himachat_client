bdfb31d452542675426d4bd52c345abead3d0476
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2022-06-15 23:34:41 +0900

    add icom & background

---
+++ pages/profile.tsx
@@ -0,0 +1,69 @@
+import type { NextPage } from 'next'
+import Header from '../components/header'
+
+import IconImage from "../public/SampleImage.jpg"
+import BackgroundImage from "../public/SampleImage2.jpg"
+
+import {
+    Stack,
+    Button,
+    Typography,
+    ImageListItem,
+    ImageList,
+    Avatar
+} from "@mui/material";
+
+// 変数がいる -> 名前が大文字アルファベットになるの相談
+type MyProfile = {
+    id: number
+    user_name: string;
+    user_id: string;
+    followers: number;
+    followed: number;
+    icon: string;
+    background: string;
+};
+
+
+const Profile: NextPage = () => {
+
+
+    const users: MyProfile[] = [
+        { id: 1, user_name: "User Name", user_id: "@user_id", followers: 999, followed: 999, icon: IconImage.src, background: BackgroundImage.src, },
+    ]
+
+
+
+    return (
+        <div>
+            <Header title={'プロフィール'} />
+
+
+
+            {users.map((user: MyProfile) => {
+
+                return (
+
+
+
+                    <Stack style={{ alignItems: 'center', justifyContent: "space-even" }} key={user.id}>
+                        <ImageListItem key={user.background} >
+                            <img src={`${user.background}`} style={{ width: 428, height: 120 }} /></ImageListItem>
+                        <Avatar alt="Icon" src={user.icon} style={{ width: 120, height: 120, borderRadius: 90, top: -65 }} />
+                        <Typography style={{ fontSize: 16 }}>{user.user_name}</Typography>
+                        <Typography style={{ color: "#808080", fontSize: 14 }}>{user.user_id}  </Typography>
+                        <Stack direction="row" >
+                            <Button variant="text" style={{ color: "black", fontSize: 14 }}>{user.followers} <span> フォロー</span> </Button>
+                            <Button variant="text" style={{ color: "black", fontSize: 14 }}>{user.followed}  <span> フォロワー</span> </Button>
+                        </Stack>
+                    </Stack>
+                )
+            })}
+
+
+        </div>
+    );
+}
+
+
+export default Profile
