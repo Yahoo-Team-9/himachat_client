e2976e278f872b376866176f240fdd781ba0c75a
Kenzo-Wada <wada-kenzo@ed.tmu.ac.jp>
2022-06-20 18:23:40 +0900

    :sparkles: add auto format

--- components/header.tsx
+++ components/header.tsx
@@ -2,20 +2,19 @@ import type { NextPage } from 'next'
 import { AppBar, Toolbar, Box, Typography } from '@mui/material'
 
 interface Props {
-    title: String
+  title: String
 }
 
-const Header: NextPage<Props> = ({title}) => {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar color='inherit' position="static" elevation={0} sx={{borderBottom: 1, borderBottomColor:'#f5f6f6'}} >
-                <Toolbar sx={{display:'flex',justifyContent: 'center'}}>
-                    <Typography sx={{fontWeight:'bold'}}>{title}</Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    )
+const Header: NextPage<Props> = ({ title }) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar color="inherit" position="static" elevation={0} sx={{ borderBottom: 1, borderBottomColor: '#f5f6f6' }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Typography sx={{ fontWeight: 'bold' }}>{title}</Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
 }
 
 export default Header
-
