5e658584ed41dae2c87fc4acf12fcab3fb380091
Keiichi-S <keiichidori1222@gmail.com>
2022-06-28 15:09:33 +0900

    debug code socket update hima status

--- pages/[chatRoom].tsx
+++ pages/[chatRoom].tsx
@@ -1,25 +1,43 @@
 import { NextPage } from "next";
 import Header from "../components/header";
 import { useEffect, useState } from "react";
-import { setupSocket, sendMessage, chatInit } from "../utils/socket";
+import { setupSocket, updateStatus} from "../utils/socket";
 import { useRouter } from "next/router";
 
+// Chat機能はなくなかったが、socket用のデバックコードとして使用
 const ChatRoom: NextPage = () => {
     const [messageList, setMessageList] = useState<string[]>([]);
     const [message, setMessage] = useState<string>("");
     const router = useRouter()
     const chatRoom = router.query.chatRoom as string;
+
+    let friend_ids: number[] = [] //ログインしたor暇じゃなくなったことを知らせたい友達. friendテーブルのfriend_idをroomのidとして使用
+
+    //以下デバッグ用
+    if(chatRoom == "1"){
+        friend_ids = [1]
+    }
+    else if (chatRoom == "2"){
+        friend_ids = [2]
+    }
+    else if (chatRoom == "3"){
+        friend_ids = [3]
+    }
+    else if (chatRoom == "4"){
+        friend_ids = [1,2]
+    }
+
     useEffect(() => {
-        setupSocket(chatRoom);
-        chatInit(setMessageList);
-    }, [chatRoom]);
+        if (friend_ids.length != 0){
+            setupSocket(friend_ids);
+        }
+    }, [friend_ids]);
     return(
         <>
             <Header title={'チャット'}/>
             <textarea value={message} onChange={(event) => setMessage(event.target.value)}></textarea>
             <button onClick={() => {
-                sendMessage(chatRoom, message)
-                setMessage("")
+                updateStatus(friend_ids)
             }}>
                 送信
             </button>
--- utils/socket.ts
+++ utils/socket.ts
@@ -2,32 +2,32 @@ import { Dispatch, SetStateAction } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 let socket: Socket;
-let chatMessages: string[] = [];
-let setChatMessages: Dispatch<SetStateAction<string[]>>;
+
+ 
 
 const setupSocket = async (
-    chatRoom: string
+    friend_ids: number[]
 ) => {
+    
+    
     socket = io("localhost:8080").connect();
-    socket.emit("join", "taro", chatRoom)
 
-    socket.on("message", handleMessage);
+    socket.emit("join", "taro", friend_ids)
+
+    socket.on("update_hima_status", handle_hima_status) //友達の誰かが、暇状態を更新したら、実行される
+
 };
 
-const chatInit = (setMessages: Dispatch<SetStateAction<string[]>>) => {
-    setChatMessages = setMessages;
-}
-
-const sendMessage = (chatRoom: string, message: string) => {
-    
-    socket.emit("message", chatRoom, message)
-}
-
-const handleMessage = (message: string) => {
-    chatMessages = chatMessages.concat(message);
-    setChatMessages(chatMessages);
+// もしログインor暇じゃなくなったら、友達に知らせるため(友達のtop画面を更新するため)に実行
+const updateStatus = (friend_ids: number[]) => {
+    socket.emit("update_hima_status", friend_ids)
 }
 
 
+const handle_hima_status = () => {
+    console.log("someone update status!!!")
+    // 誰かがログインor暇じゃなくなったから、fetchしてtop画面を更新する
+}
 
-export {setupSocket, chatInit, sendMessage}
+
+export {setupSocket, updateStatus}
