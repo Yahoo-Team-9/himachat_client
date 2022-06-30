a8515f9bd6f5fa3c213021365b42596c51d05ac1
Keiichi-S <keiichidori1222@gmail.com>
2022-06-30 14:10:23 +0900

    fix

--- pages/top.tsx
+++ pages/top.tsx
@@ -19,8 +19,6 @@ import {
 } from '@mui/material'
 import { useState, useEffect } from 'react'
 import { useSession, signIn } from 'next-auth/react'
-import { borderRadius } from '@mui/system'
-import { VerticalAlignTop } from '@mui/icons-material'
 import { io, Socket } from 'socket.io-client';
 
 type MyProfile = {
@@ -99,10 +97,6 @@ const Nortification: NextPage = () => {
     })
       .then((res) => res.json())
       .then((data) => setHimaFriends(data))
-<<<<<<< HEAD
-  }, [])
-      
-=======
     
     fetch(SERVER_URL + "api/leisure/set_leisure", {
       method: 'POST',
@@ -114,7 +108,6 @@ const Nortification: NextPage = () => {
       .then((data) => console.log(data))
   }, [])
 
->>>>>>> fetch set_leisure
   useEffect(() => {
     fetch(SERVER_URL + "api/friend/get_friend_list", {
       method: 'POST',
@@ -143,33 +136,32 @@ const Nortification: NextPage = () => {
     return (
       <div>
         <Header title={'フォロー中'} />
-
-          return (
-            <List sx={{ width: '100%', bgcolor: '#fff', height: 80 }} disablePadding key={myProfile["user_profiles"][0][0]}>
-              <ListItem alignItems="flex-start" disablePadding>
-                <ListItemAvatar style={{ paddingLeft: 16, paddingRight: 16, marginTop: 15 }}>
-                  <Avatar alt='Icon' src={IconImage.src} style={{ borderRadius: 10, height: 48, width: 48 }} />
+        <List sx={{ width: '100%', bgcolor: '#fff', height: 80 }} disablePadding key={myProfile["user_profiles"][0][0]}>
+          <ListItem alignItems="flex-start" disablePadding>
+            <ListItemAvatar style={{ paddingLeft: 16, paddingRight: 16, marginTop: 15 }}>
+              <Avatar alt='Icon' src={IconImage.src} style={{ borderRadius: 10, height: 48, width: 48 }} />
           {/* <Avatar alt='Icon' src={myProfile.icon} style={{ borderRadius: 10, height: 48, width: 48 }} /> */}
-                <ListItemText
-                  style={{ marginTop: 20 }}
-                  primary={myProfile["user_profiles"][0][1]}
-                  primaryTypographyProps={{
-                    fontWeight: 'medium',
-                    fontSize: '14px',
-                  }}
-                  secondary={"login at " + new Date(myProfile["user_profiles"][0][5]).getHours() + ":" + new Date(myProfile["user_profiles"][0][5]).getMinutes()}
-                  secondaryTypographyProps={{
-                    fontWeight: 'medium',
-                    fontSize: '12px',
-                    display: 'inline',
-                  }}
-                />
-              </ListItem>
-            </List>
+            </ListItemAvatar>
+            <ListItemText
+              style={{ marginTop: 20 }}
+              primary={myProfile["user_profiles"][0][1]}
+              primaryTypographyProps={{
+                fontWeight: 'medium',
+                fontSize: '14px',
+              }}
+              secondary={"login at " + new Date(myProfile["user_profiles"][0][5]).getHours() + ":" + new Date(myProfile["user_profiles"][0][5]).getMinutes()}
+              secondaryTypographyProps={{
+                fontWeight: 'medium',
+                fontSize: '12px',
+                display: 'inline',
+              }}
+            />
+          </ListItem>
+        </List>
 
         <Stack style={{ alignItems: 'center', justifyContent: 'space-even', marginBottom: 80 }}>
           <ImageList sx={{ height: 1230 }} cols={2} rowHeight={164}>
-            {friends.map((friend: OthersNotification) => (
+            {himaFriends.map((friend: OthersNotification) => (
               <ImageListItem key={friend.friend} style={{ alignItems: 'center', justifyContent: 'space-even' }}>
                 <ListItemButton onClick={handleOpen}>
                   {/* <img src={`${friend.icon}`} style={{ width: 150, height: 150, borderRadius: 10}} /> */}
@@ -191,19 +183,8 @@ const Nortification: NextPage = () => {
             userid={'@user_id'}
             follow={123}
             follower={123}
-          />
-
-        <ProfileModal
-          open={open}
-          handleClose={handleClose}
-          name={'User Name'}
-          icon={IconImage.src}
-          background={BackgroundImage.src}
-          userid={'@user_id'}
-          follow={123}
-          follower={123}
         />
-
+        
         <Footer homeiconcolor="#141D26" belliconcolor="#808080" iconcolor="#808080" />
       </div>
     )
