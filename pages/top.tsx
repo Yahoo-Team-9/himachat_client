c43265720fa62f2ad11073714eb1a2772e502140
Keiichi-S <keiichidori1222@gmail.com>
2022-07-01 01:05:45 +0900

    complete main_logic

--- pages/top.tsx
+++ pages/top.tsx
@@ -33,15 +33,16 @@ type OthersNotification = {
   friend: number //primary_user_id
   user_id: string
   login_at: Date
+  user_name: string
+  bio: string
+  tag_list: string[]
+  friend_list: number[]
   // icon: string
   // background: string
 }
 const AZURE_URL = "https://himathing.azurewebsites.net/"
 const LOCAL_URL = "http://localhost:8080/"
-const SERVER_URL = AZURE_URL
-
-const PRIMARY_USER_ID = 1
-
+const SERVER_URL = LOCAL_URL
 
 
 let socket: Socket;
@@ -50,13 +51,12 @@ const Nortification: NextPage = () => {
   const [myProfile, setMyProfile] = useState<MyProfile>({"icon_path": "./", "user_profiles": [[]]});
   const [himaFriends, setHimaFriends] = useState<OthersNotification[]>([]);
   const [friends, setFriends] = useState<OthersNotification[]>([]);
-  const [modalData, setModalData] = useState<OthersNotification>({"friend_id": -1, "friend": -1, user_id: "-1", "login_at": new Date()});
-
+  const [modalData, setModalData] = useState<OthersNotification>({ "friend_id": -1, "friend": -1, user_id: "-1", "login_at": new Date(), "user_name": "test", "bio": "aaa", "tags": [], "friend_list": [] });
   const [open, setOpen] = useState(false)
-  const handleOpen = (friend: OthersNotification) => {
-    setOpen(true)
-    setModalData(friend)
-  }
+  // const handleOpen = (friend: OthersNotification) => {
+  //   setOpen(true)
+  //   setModalData(friend)
+  // }
   const handleClose = () => setOpen(false)
   const { data: session } = useSession()
 
@@ -70,61 +70,66 @@ const Nortification: NextPage = () => {
   };
 
   const handle_hima_status = () => {
+    if (session) {
       fetch(SERVER_URL + "api/friend/get_hima_friend_list", {
       method: 'POST',
       headers: {
         'Content-Type': "application/json",
         },
-      body: JSON.stringify({"primary_user_id": PRIMARY_USER_ID})
+      body: JSON.stringify({"primary_user_id": session["primary_user_id"]})
     })
       .then((res) => res.json())
       .then((data) => setHimaFriends(data))
+    }
   }
 
   useEffect(() => {
-    // 自分のプロフィールを取得
-    fetch(SERVER_URL + "api/user/get_profile", {
-      method: 'POST',
-      headers: {
-        'Content-Type': "application/json",
-        },
-      body: JSON.stringify({"primary_user_id": PRIMARY_USER_ID})
-    })
-      .then((res) => res.json()) 
-      .then((data) => setMyProfile(data))
+    if (session) {
+       // 自分のプロフィールを取得
+      fetch(SERVER_URL + "api/user/get_profile", {
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json",
+          },
+        body: JSON.stringify({"primary_user_id": session["primary_user_id"]})
+      })
+        .then((res) => res.json()) 
+        .then((data) => setMyProfile(data))
 
-    // 暇な友達を取得
-    fetch(SERVER_URL + "api/friend/get_hima_friend_list", {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': "application/json",
-        },
-      body: JSON.stringify({"primary_user_id": PRIMARY_USER_ID})
-    })
-      .then((res) => res.json())
-      .then((data) => setHimaFriends(data))
-    
-    // 自分のログイン時間を更新
-    fetch(SERVER_URL + "api/leisure/set_leisure", {
-      method: 'POST',
-      headers: {
-        'Content-Type': "application/json",
-        }
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
+      // 暇な友達を取得
+      fetch(SERVER_URL + "api/friend/get_hima_friend_list", {
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json",
+          },
+        body: JSON.stringify({"primary_user_id": session["primary_user_id"]})
+      })
+        .then((res) => res.json())
+        .then((data) => setHimaFriends(data))
+      
+      // 自分のログイン時間を更新
+      fetch(SERVER_URL + "api/leisure/set_leisure", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': "application/json charset=utf-8",
+          }
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+      }
   }, [])
 
 
   // 友達に対してwebsocketを確立
   useEffect(() => {
-    fetch(SERVER_URL + "api/friend/get_friend_list", {
+    if (session) {
+      fetch(SERVER_URL + "api/friend/get_friend_list", {
       method: 'POST',
       headers: {
         'Content-Type': "application/json",
       },
-      body: JSON.stringify({ "primary_user_id": PRIMARY_USER_ID})
+      body: JSON.stringify({ "primary_user_id": session["primary_user_id"]})
     })
       .then((res) => res.json())
       .then((data) => setFriends(data))
@@ -134,6 +139,7 @@ const Nortification: NextPage = () => {
       setupSocket(myProfile["user_profiles"][0][1], friend_ids)
       // 友達に自分がログインしたことを通知
       socket.emit("update_hima_status", friend_ids)
+    }
     }
   }, [friends])
 
@@ -149,12 +155,12 @@ const Nortification: NextPage = () => {
             </ListItemAvatar>
             <ListItemText
               style={{ marginTop: 20 }}
-              primary={myProfile["user_profiles"][0][1]}
+              primary={myProfile["user_profiles"][0][2]}
               primaryTypographyProps={{
                 fontWeight: 'medium',
                 fontSize: '14px',
               }}
-              secondary={"login at " + new Date(myProfile["user_profiles"][0][5]).getHours() + ":" + new Date(myProfile["user_profiles"][0][5]).getMinutes()}
+              secondary={new Date(Date.now() + ((new Date(myProfile["user_profiles"][0][5]).getTimezoneOffset() + (9 * 60)) * 60 * 1000)).getHours() + ":" + new Date(Date.now() + ((new Date(myProfile["user_profiles"][0][5]).getTimezoneOffset() + (9 * 60)) * 60 * 1000)).getMinutes() + " に暇になった"}
               secondaryTypographyProps={{
                 fontWeight: 'medium',
                 fontSize: '12px',
@@ -168,12 +174,15 @@ const Nortification: NextPage = () => {
           <ImageList sx={{ height: 123* (himaFriends.length + 1) }} cols={2} rowHeight={164}>
             {himaFriends.map((friend: OthersNotification) => (
               <ImageListItem key={friend.friend} style={{ alignItems: 'center', justifyContent: 'space-even' }}>
-                <ListItemButton>
+                <ListItemButton onClick={() => {
+                  setOpen(true)
+                  setModalData(friend)
+                }}>
                   {/* <img src={`${friend.icon}`} style={{ width: 150, height: 150, borderRadius: 10}} /> */}
                 <img src={IconImage.src} style={{ width: 150, height: 150, borderRadius: 10}} />
                 </ListItemButton>
-                <Typography style={{ fontSize: 16, color: '#141D26' }}>{friend.user_id}</Typography>
-                <Typography style={{ color: '#808080', fontSize: 12 }}>{"login at " + new Date(friend.login_at).getHours() + ":" + new Date(friend.login_at).getMinutes()} </Typography>
+                <Typography style={{ fontSize: 16, color: '#141D26' }}>{friend.user_name}</Typography>
+                <Typography style={{ color: '#808080', fontSize: 12 }}>{new Date(Date.now() + ((new Date(friend.login_at).getTimezoneOffset() + (9 * 60)) * 60 * 1000)).getHours() + ":" + new Date(Date.now() + ((new Date(friend.login_at).getTimezoneOffset() + (9 * 60)) * 60 * 1000)).getMinutes() + " に暇になった"} </Typography>
               </ImageListItem>
             ))}
           </ImageList>
@@ -182,13 +191,13 @@ const Nortification: NextPage = () => {
         <ProfileModal
             open={open}
             handleClose={handleClose}
-            name={'User Name'}
+            name={modalData.user_name}
             icon={IconImage.src}
             background={BackgroundImage.src}
-            userid={'@user_id'}
-            friendNumber={10}
-            bio={"ああああ"}
-            tags={["渋谷", "ランチ"]}
+            userid={modalData.user_id}
+            friendNumber={modalData.friend_list.length}
+            bio={modalData.bio}
+            tag_list={modalData.tag_list}
             isFriend={true}
         />
         
