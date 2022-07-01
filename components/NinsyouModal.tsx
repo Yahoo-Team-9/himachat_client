cff262adebc5bc9278ea0a70b385b5fb9436a368
Momo227 <uhgrie7gt3kgo384uygr@gmail.com>
2022-07-01 19:22:45 +0900

    add components/NinsyouModal.tsx

--- components/NinsyouModal.tsx
+++ components/NinsyouModal.tsx
@@ -1,19 +1,7 @@
 import { NextPage } from "next";
-import { Modal, Stack, Paper, IconButton, InputBase, List, Avatar, Input, Typography, Checkbox, Button, Box } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import Header from '../components/header'
-import IconImage from '../public/SampleImage.jpg'
-import { Scale } from "@mui/icons-material";
-import CancelIcon from '@mui/icons-material/Cancel';
+import { Modal, Stack, Button, Typography, List } from "@mui/material";
 
-type UserList = {
-  id: number
-  name: string
-  icon: string
-  userid: string
-}
-  
-    
+
 interface Props{
     open: any;
     handleClose: any;
@@ -21,153 +9,70 @@ interface Props{
     icon: string;
     name: string;
     userid: string;
-    follower: number;
-    follow: number
+    friendNumber: number;
+    bio: string;
+    tag_list: string[];
+    isFriend: boolean;
+    primary_user_id: number;
 }
 
-const NinsyouModal: NextPage<Props> = ({ open, handleClose, background, icon, name, userid, follower, follow }) => {
-    
-    const users: UserList[] = [
-    {
-      id: 1,
-      name: 'python',
-      icon: IconImage.src,
-      userid: '@ume',
-    },
-    {
-      id: 2,
-      name: 'java',
-      icon: IconImage.src,
-      userid: '@0kaka',
-        },
-    {
-      id: 3,
-      name: 'C#',
-        icon: IconImage.src,
-      userid: '@kimuchI',
-    },
-    {
-      id: 4,
-      name: 'C++',
-        icon: IconImage.src,
-      userid: '@k0nbU',
-        },
-    {
-      id: 5,
-      name: 'Vue',
-        icon: IconImage.src,
-      userid: '@syake',
-    },
-    {
-      id: 6,
-      name: 'C',
-        icon: IconImage.src,
-      userid: '@27',
-        },
-    {
-      id: 7,
-      name: 'pytorch',
-      icon: IconImage.src,
-      userid: '@s0lt',
-    },
-    {
-      id: 8,
-      name: 'javaScript',
-      icon: IconImage.src,
-      userid: '@noRi',
-    },
-    {
-      id: 9,
-      name: 'HTML',
-      icon: IconImage.src,
-      userid: '@UmEoKaKa',
-    },
-    {
-      id: 10,
-      name: 'css',
-      icon: IconImage.src,
-      userid: '@KATSUO',
-        },
-    {
-      id: 11,
-      name: 'TypeScript',
-      icon: IconImage.src,
-      userid: '@sio',
-        },
-    {
-      id: 12,
-      name: 'Tensorflow',
-      icon: IconImage.src,
-      userid: '@nAtto',
-    },
-    ]
-    
-    const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+const AZURE_URL = "https://himathing.azurewebsites.net/"
+const LOCAL_URL = "http://localhost:8080/"
+const SERVER_URL = AZURE_URL
 
-    
+const ProfileModal: NextPage<Props> = ({ open, handleClose, background, icon, name, userid, friendNumber, bio, tag_list, isFriend, primary_user_id }) => {
+    const handleFriendRequest = () => {
+        fetch(SERVER_URL + "api/friend/send_friend_req", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': "application/json charset=utf-8",
+        },
+      body: JSON.stringify({
+        "friend": primary_user_id
+      })
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+    }
     return (
-        
-        <Modal open={open} onClose={handleClose}>
-            
-        <Stack style={{
-            alignItems: 'center',
-            justifyContent: "space-even",
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: "80%",
-            backgroundColor: 'white',
-            borderRadius: 10,
-            }} >
-        
-        <List sx={{ width: '100%', bgcolor: '#fff', height: 50, borderRadius: 10 }} >
-        <Typography style={{ fontSize: 16,  textAlign:'center', fontWeight: 'bold', paddingTop:30 }}>グループ</Typography>
-        </List>
-
-        <Paper component="form" sx={{ display: 'flex', alignItems: 'center', height: 20, marginTop: 3, width: '90%'}} >
-        <SearchIcon />
-         <InputBase sx={{ ml: 5, flex: 1 }} inputProps={{ 'aria-label': ' ' }} id="message" />
-        
-        </Paper>
-
-         <List sx={{ width: '95%', bgcolor: '#F5F6F6', height: 50, marginTop: 2 ,overflowX: 'scroll'}} >
-         <Stack direction="row" >
-         {users.map((user: UserList) => {
-            return (
-            <Stack direction="row" key={user.id} >
-            <Avatar alt="Icon" src={user.icon} style={{ borderRadius: 5, marginLeft: 15, height: 24, width: 24, marginTop: 5 }}  />
-            <IconButton aria-label="cancel" >
-            <CancelIcon fontSize="small" style={{ marginTop: -15, marginLeft: -18}} color='warning'/>
-            </IconButton>
-            </Stack> 
-        )   
-        })}
-        </Stack> 
-        </List>
-                
-        <Box sx={{ width: '100%', height: 350, bgcolor: '#FFFFFF', overflowY: 'scroll', marginTop: 1 }}>
-
-        {users.map((user: UserList) => {
-            return (
-        <List sx={{ width: '100%', bgcolor: '#FFFFFF', height: 60,}} key={user.id}  >
-        <Stack spacing={2} direction="row" >
-            <Avatar alt="Icon" src={user.icon} style={{ borderRadius: 10, marginLeft: 15}} />
-            <Typography style={{ fontSize: 16, color: '#000000', marginLeft: 30, marginTop: 10 }}>{user.name}</Typography>
-            <Checkbox {...label} style={{marginLeft: 'auto',  marginRight: 20, color: '#4BB543'}} sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />
-        </Stack>
-        </List>
-        ) 
-        })}
+        <>
+            <Modal open={open} onClose={handleClose}>
+                <Stack style={{
+                    alignItems: 'center',
+                    justifyContent: "space-even",
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: "80%",
+                    backgroundColor: 'white',
+                    borderRadius: 10
+                }} >
                     
-         </Box>
-
-         <Button variant="outlined" style={{ color: '#DD5144', marginLeft: 'auto', borderColor: "#DD5144", marginBottom: 20, fontSize: 16, height:30, marginRight: 30, marginTop: 10 }}>作る</Button>
+                    <List sx={{ width: '100%', bgcolor: '#fff', height: 50, borderRadius: 10 }} >
+                    <Typography style={{ fontSize: 16,  textAlign:'center', fontWeight: 'bold', paddingTop:10 }}>友達申請が来ました</Typography>
+                    </List>
                     
-        </Stack>
-         </Modal>
-        
+                    <img src={`${background}`} style={{width:'75%',borderTopLeftRadius: 10, borderTopRightRadius: 10, height: 100 }} />
+                    <img alt="Icon" src={icon} style={{ width: 100, height: 100, borderRadius: 90, top: 95, position: 'absolute'}} />
+                    <Typography style={{ fontSize: 16 , marginTop: 60}}>{name}</Typography>
+                    <Typography style={{ color: "#808080", fontSize: 14 }}>{userid}  </Typography>
+                    <Typography style={{ color: "#808080", fontSize: 14 }}>{bio}  </Typography>
+                    
+                    <Stack direction="row" spacing={1}>
+                        <Button variant="text" style={{ color: "#141D26", fontSize: 14 }}>{friendNumber} <span> 友達</span> </Button>
+                    </Stack>
+                    <Stack direction="row" spacing={5} style={{ margin: 20 }}>
+                    <Button variant="outlined" style={{ fontWeight:'bold', color:"#DD5144", borderColor: "#DD5144", fontSize: 14 }} onClick={handleFriendRequest}>許可 </Button> 
+                    <Button variant="outlined" style={{ fontWeight:'bold', color:"#DD5144", borderColor: "#DD5144", fontSize: 14 }} onClick={handleFriendRequest}>拒否 </Button>  
+                    </Stack>
+                    
+
+                </Stack>
+            </Modal>
+        </>
     )
 }
 
-export default NinsyouModal
+export default ProfileModal
